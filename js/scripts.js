ea9aa5747926d7016234f91400228a426226db5a
Jaki <jaki.widby@gmail.com>
2021-09-27 14:09:15 -0700

    create function to toggle menu.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi there, thanks for checking out my portfolio site.");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
