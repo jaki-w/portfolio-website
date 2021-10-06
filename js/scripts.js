bf0d485f79378371013d3b279831db68138a04a3
Jaki <jaki.widby@gmail.com>
2021-10-06 09:19:19 -0700

    fix linter errors
    remove extra semi-colons and add spaces before parenthesis.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
-console.log("Hi there, thanks for checking out my portfolio site.");
+console.log('Hi there, thanks for checking out my portfolio site.')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-    $(".topBarSocialIcon").hide();
+    x.className += ' responsive'
+    $('.topBarSocialIcon').hide()
   } else {
-    x.className = 'navtoggle';
-    $(".topBarSocialIcon").hide();
+    x.className = 'navtoggle'
+    $('.topBarSocialIcon').hide()
   }
 };
 
-function bottomMenuToggle() {
-  var x = document.getElementById('myBottomNavToggle');
+function bottomMenuToggle () {
+  var x = document.getElementById('myBottomNavToggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-    $("#itemName").html("Table of Contents");
+    x.className += ' responsive'
+    $('#itemName').html('Table of Contents')
   } else {
-    x.className = 'navtoggle';
-    $("#itemName").html("Table of Contents");
+    x.className = 'navtoggle'
+    $('#itemName').html('Table of Contents')
   }
 };
 
-function displayItemName(itemName) {
-  $("#itemName").html(itemName);
-  $("#itemName").show();
+function displayItemName (itemName) {
+  $('#itemName').html(itemName)
+  $('#itemName').show()
 };
